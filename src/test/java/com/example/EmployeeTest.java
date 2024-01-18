a8e3483e4794cc88e5efc53d361c190408512c24
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-01-18 04:36:34 +0100

    Three tests are written constroctors, setId and getId

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -6,8 +6,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
+    Employee employee= new Employee("111",10000);
+    @Test
+    @DisplayName("An employee is created when the method is called")
+    void anEmployeeIsCreatedWhenTheMethodIsCalled() {
+        assertInstanceOf (Employee.class,employee);
+    }
+
+    @Test
+    @DisplayName("ID is given when there is an employee")
+    void idIsGivenWhenThereIsAnEmployee() {
+        assertEquals("111",employee.getId());
+    }
+
+    @Test
+    @DisplayName("Id is changed to a new id ")
+    void idIsChangedToANewId() {
+        employee.setId("222");
+        assertEquals("222",employee.getId());
+    }
+
 
 }
-
-
-
