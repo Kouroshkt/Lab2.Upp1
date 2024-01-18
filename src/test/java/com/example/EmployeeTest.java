80ec9e69dc7795798b415ab0c318cdeebe70c2cd
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-01-18 04:58:43 +0100

    Add test for setSalary and getSalary

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -6,25 +6,55 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
-    Employee employee= new Employee("111",10000);
+    Employee employee = new Employee("111", 10000);
+    Employee noBady = new Employee(null, 0);
+
     @Test
-    @DisplayName("An employee is created when the method is called")
-    void anEmployeeIsCreatedWhenTheMethodIsCalled() {
-        assertInstanceOf (Employee.class,employee);
+    @DisplayName("An employee is created when the constructor is called")
+    void anEmployeeIsCreatedWhenTheConstructorIsCalled() {
+        assertInstanceOf(Employee.class, employee);
     }
+
+    @Test
+    @DisplayName("An empty employee is created when constructor is called")
+    void anEmptyEmployeeIsCreatedWhenConstructorIsCalled() {
+        assertInstanceOf(Employee.class, noBady);
+    }
+
 
     @Test
     @DisplayName("ID is given when there is an employee")
     void idIsGivenWhenThereIsAnEmployee() {
-        assertEquals("111",employee.getId());
+        assertEquals("111", employee.getId());
     }
 
     @Test
-    @DisplayName("Id is changed to a new id ")
-    void idIsChangedToANewId() {
+    @DisplayName("Id is given when id is null")
+    void idIsGivenWhenIdIsNull() {
+        assertNull(noBady.getId());
+    }
+
+
+    @Test
+    @DisplayName("Should Id is changed to a new id ")
+    void shouldIdIsChangedToANewId() {
         employee.setId("222");
-        assertEquals("222",employee.getId());
+        assertEquals("222", employee.getId());
+    }
+
+    @Test
+    @DisplayName("Salary is given when there is an employee")
+    void salaryIsGivenWhenThereIsAnEmployee() {
+        assertEquals(10000, employee.getSalary());
+    }
+
+    @Test
+    @DisplayName("Should salary is changed to a new salary")
+    void shouldSalaryIsChangedToANewSalary() {
+        employee.setSalary(20000);
+        assertEquals(20000, employee.getSalary());
     }
 
 
 }
+
