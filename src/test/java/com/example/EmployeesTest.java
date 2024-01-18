c5826cb83063eb4d6d52c11e1d4bda632d09472c
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-01-18 13:29:07 +0100

    Create EmployeesTest and DummyBankSerivce

---
+++ src/test/java/com/example/EmployeesTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class EmployeesTest {
+
+}
