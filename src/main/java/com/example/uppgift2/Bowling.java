c55f33d6b152bd649d06cd57a227d87c715f8a35
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-01-20 09:34:25 +0100

    Add second test

--- src/main/java/com/example/uppgift2/Bowling.java
+++ src/main/java/com/example/uppgift2/Bowling.java
@@ -1,4 +1,18 @@
 package com.example.uppgift2;
 
 public class Bowling {
+    int pOneScore;
+    int pTwoScore;
+
+    public String score() {
+        return pOneScore + "-" + pTwoScore;
+    }
+
+    public void playerOneScore() {
+        pOneScore++;
+    }
+
+    public void playerTwoScore() {
+        pTwoScore++;
+    }
 }
--- src/test/java/com/example/uppgift2/BowlingTest.java
+++ src/test/java/com/example/uppgift2/BowlingTest.java
@@ -1,5 +1,26 @@
 package com.example.uppgift2;
 
+import org.assertj.core.api.AbstractBigDecimalAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BowlingTest {
+
     Bowling bowling= new Bowling();
+
+    @Test
+    @DisplayName("Given 0-0 when the game has not started")
+    void given0WhenTheGameHasNotStarted() {
+        assertThat(bowling.score()).isEqualTo("0-0");
+    }
+
+    @Test
+    @DisplayName("Given 1-0 when player one knock down one pin")
+    void given1WhenPlayerOneKnockDownOnePin() {
+        bowling.playerOneScore();
+        assertThat(bowling.score()).isEqualTo("1-0");
+    }
+
 }
