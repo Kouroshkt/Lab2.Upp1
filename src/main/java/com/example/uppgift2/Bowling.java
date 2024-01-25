d321a65136e38c39b7a4e2dcd13b6fb243205d5a
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-01-25 16:08:53 +0100

    Refaktorisera kod

--- src/main/java/com/example/uppgift2/Bowling.java
+++ src/main/java/com/example/uppgift2/Bowling.java
@@ -19,13 +19,16 @@ public class Bowling {
 
     public void roll(int knockDownPinPlayer) {
         rollInFrame++;
-        if (!onFrame) {
-            rollInFrame1 = knockDownPinPlayer;
-            onFrame = true;
-        } else {
-            rollInFrame2 = knockDownPinPlayer;
-            onFrame = false;
-        }
+        rollInFrameNumber(knockDownPinPlayer);
+        knockDownPinPlayer = bonusCalculation(knockDownPinPlayer);
+        sumScore(knockDownPinPlayer);
+        givenABonus();
+        resetRollInFrame();
+        bonusInFrame10();
+    }
+
+
+    private int bonusCalculation(int knockDownPinPlayer) {
         if (spare) {
             knockDownPinPlayer *= 2;
             spare = false;
@@ -33,9 +36,29 @@ public class Bowling {
             knockDownPinPlayer *= 2;
             if (rollInFrame == 2) strike = false;
         }
-        score = knockDownPinPlayer + score;
-        playerScore = score;
+        return knockDownPinPlayer;
+    }
 
+
+    private void bonusInFrame10() {
+        if ((spare || strike) && frame == 9) {
+            strike = false;
+            spare = false;
+            rollInFrame=-1;
+        }
+    }
+
+    private void resetRollInFrame() {
+        if (rollInFrame == 2) {
+            rollInFrame = 0;
+            rollInFrame1 = 0;
+            rollInFrame2 = 0;
+            frame++;
+            if(frame==10)endGame=true;
+        }
+    }
+
+    private void givenABonus() {
         if (rollInFrame1 == 10 && rollInFrame == 1) {
             strike = true;
             rollInFrame = 2;
@@ -44,17 +67,20 @@ public class Bowling {
         } else if (rollInFrame1 + rollInFrame2 == 10) {
             spare = true;
         }
-        if (rollInFrame == 2) {
-            rollInFrame = 0;
-            rollInFrame1 = 0;
-            rollInFrame2 = 0;
-            frame++;
-            if(frame==10)endGame=true;
-        }
-        if ((spare || strike) && frame == 9) {
-            strike = false;
-            spare = false;
-            rollInFrame=-1;
+    }
+
+    private void sumScore(int knockDownPinPlayer) {
+        score = knockDownPinPlayer + score;
+        playerScore = score;
+    }
+
+    private void rollInFrameNumber(int knockDownPinPlayer) {
+        if (!onFrame) {
+            rollInFrame1 = knockDownPinPlayer;
+            onFrame = true;
+        } else {
+            rollInFrame2 = knockDownPinPlayer;
+            onFrame = false;
         }
     }
 }
